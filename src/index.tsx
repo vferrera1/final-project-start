4bcb9d783773ebe885afdf2369127547be0ea446
vfly942 <vaflyers@gmail.com>
2022-11-01 15:21:55 -0400

    Index should render with respect to App. Commented out code that renders the drag & drop example.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Board from "./Board";
+import App from "./App";
+//import Board from "./Board";
 import "./index.css";
-import { observe } from "./game";
+//import { observe } from "./game";
 import reportWebVitals from "./reportWebVitals";
 
-observe((picPosition: [number, number]) => {
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
+/*observe((picPosition: [number, number]) => {
     ReactDOM.render(
         <React.StrictMode>
             <div
@@ -21,7 +28,7 @@ observe((picPosition: [number, number]) => {
         document.getElementById("root")
     );
 });
-
+*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

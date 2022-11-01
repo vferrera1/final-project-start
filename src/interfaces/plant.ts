cbe7481aaa65888a6f1379860012f884731a462e
vfly942 <vaflyers@gmail.com>
2022-11-01 10:17:23 -0400

    Added Plant and Garden interface files Wrote an unofficial Plant interface

---
+++ src/interfaces/plant.ts
@@ -0,0 +1,25 @@
+// A representation of a singular plant
+export interface Plant {
+    // The species/name of the plant (can be used as plant ID)
+    species: string;
+    // A URL/filepath of the top view of the plant
+    topImage: string;
+    // A URL/filepath of the side view of the plant
+    sideImage: string;
+    // The relative size of plant to the garden
+    size: number;
+    // A list of requirements needed to maintain plant health
+    lightRequirement: string; //Enumeration (e.g.) {0-No light, 1-Little light, 2-Some light, 3-Direct light}
+    waterRequirement: string;
+    soilRequirement: string; //Could also be called nutrient requirement
+    temperatureRequirement: string;
+    // Information regarding the bloom of the plant
+    timeToBloom: number;
+    bloomDuration: number;
+    // Current age of plant
+    age: number;
+    // Answers (yes/no) whether the plant is food for insects, birds, or other animals
+    insectFood: boolean;
+    birdFood: boolean;
+    animalFood: boolean;
+}

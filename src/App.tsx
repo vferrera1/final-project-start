a0599193789185f30a7f9342b4735baceec6fac8
vfly942 <vaflyers@gmail.com>
2022-11-01 10:47:58 -0400

    Edited Plant interface and App Created a skeleton for the first plant to be displayed in App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import "./App.css";
+import { Plant } from "./interfaces/plant";
 
 function App(): JSX.Element {
+    const firstPlant: Plant = {
+        species: "tomato",
+        topImage: "",
+        sideImage: "",
+        size: 0, // Some number / garden.size
+        lightRequirement: "", // Look up tomato light requirement
+        waterRequirement: "", // Look up tomato water requirement
+        soilRequirement: "", // Look up tomato soil requirement
+        temperatureRequirement: "", // Look up tomato temperature requirement
+        timeToBloom: 0, // Look up tomato time to bloom
+        bloomDuration: 0, // Look up tomato bloom duration
+        age: 2,
+        insectFood: true, //Verify that insects like this plant
+        birdFood: false,
+        animalFood: false // Verify that animals don't like this plant
+    };
     return (
         <div className="App">
             <header className="App-header">
@@ -11,6 +28,7 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <p>Hello, world!</p>
         </div>
     );
 }

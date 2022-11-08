f6f797a476f943329dd348f2be36d61aeb76e683
Chiku <cbhatt@udel.edu>
2022-11-08 17:59:38 -0500

    how to render something on page

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import App from "./App";
+import App from "./App";
 import Board from "./Board";
 import "./index.css";
 import { observe } from "./game";
 import reportWebVitals from "./reportWebVitals";
 
-/*ReactDOM.render(
+ReactDOM.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
     document.getElementById("root")
 );
-*/
-observe((picPosition: [number, number]) => {
+
+/*observe((picPosition: [number, number]) => {
     ReactDOM.render(
         <React.StrictMode>
             <div
@@ -28,7 +28,7 @@ observe((picPosition: [number, number]) => {
         </React.StrictMode>,
         document.getElementById("root")
     );
-});
+});*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

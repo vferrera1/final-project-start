2990348ab2b3af1f74fd1fdcf0922b83bc66321b
RobertJPerez <robertjperez07@yahoo.com>
2022-11-21 17:42:02 -0500

    List Functions:State Not Working?

--- src/components/PropList.tsx
+++ src/components/PropList.tsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 
 function PropList() {
-    const myArray = ["Plant1", "Plant2", "Plant3", "Plant4", "Plant5"];
+    const [prop, setProp] = useState<string[]>([
+        "BPlant1",
+        "APlant2",
+        "ZPlant3",
+        "CPlant4",
+        "TPlant5"
+    ]);
+
+    function generateList(prop: string[]) {
+        return prop.map((o) => <li key={o}>{o}</li>);
+    }
+    function alphabeticalOrder() {
+        setProp(prop.sort((a: string, b: string) => (a < b ? -1 : 1)));
+        console.log(prop, "Test Alpha");
+        generateList(prop);
+        return prop;
+    }
+    function ReversealphabeticalOrder() {
+        setProp(prop.sort((a: string, b: string) => (a > b ? -1 : 1)));
+        console.log(prop, "Test Rev Alpha");
+        return prop;
+    }
 
     return (
-        <ul>
-            {myArray.map((o) => (
-                <li key={o}>{o}</li>
-            ))}
-        </ul>
+        <div>
+            <strong>Prop List</strong>
+            <ul>{generateList(prop)}</ul>
+            <Button onClick={() => setProp(alphabeticalOrder())}>
+                Alphabetical Order
+            </Button>
+            <Button onClick={() => setProp(ReversealphabeticalOrder())}>
+                Reverse Alphabetical Order
+            </Button>
+        </div>
     );
 }
 

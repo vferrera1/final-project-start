d839014d2230e6405b39b91f54e0a0dd3e10ba96
Chiku <cbhatt@udel.edu>
2022-12-03 17:26:30 -0500

    update zoom 1

--- src/App.tsx
+++ src/App.tsx
@@ -21,10 +21,25 @@ function App(): JSX.Element {
                 <div className="boxcontainer">
                     <PropList></PropList>
                     <BorderBox></BorderBox>
-                    <TransformWrapper>
-                        <TransformComponent>
-                            <Garden></Garden>
-                        </TransformComponent>
+                    <TransformWrapper
+                        initialScale={1}
+                        initialPositionX={200}
+                        initialPositionY={100}
+                    >
+                        {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                            <React.Fragment>
+                                <div className="tools">
+                                    <button onClick={() => zoomIn()}>+</button>
+                                    <button onClick={() => zoomOut()}>-</button>
+                                    <button onClick={() => resetTransform()}>
+                                        x
+                                    </button>
+                                </div>
+                                <TransformComponent>
+                                    <Garden></Garden>
+                                </TransformComponent>
+                            </React.Fragment>
+                        )}
                     </TransformWrapper>
                     <BorderBox></BorderBox>
                 </div>
